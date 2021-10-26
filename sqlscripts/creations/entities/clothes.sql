f6e38f819ac0b15557802d54fcd6b6645a4ad481
Mike <lavlinskiy2001@mail.ru>
2021-10-26 17:04:36 +0300

    stidno

--- sqlscripts/creations/entities/clothes.sql
+++ sqlscripts/creations/entities/clothes.sql
@@ -8,10 +8,5 @@ CREATE TABLE IF NOT EXISTS clothes (
     FOREIGN KEY( fk_colour_name ) REFERENCES colours( name ),
     density integer NOT NULL,
     fk_type_name text NOT NULL,
-<<<<<<< HEAD
     FOREIGN KEY( fk_type_name ) REFERENCES types( name )
 );
-=======
-    FOREIGN KEY( fk_type_name ) REFERENCES types( name ) 
-);
->>>>>>> 2ae5f5d2fbb5d04e0f74be5df4942ec63f2e8aea
--- sqlscripts/insertions/clothes.sql
+++ sqlscripts/insertions/clothes.sql
@@ -4,10 +4,4 @@ INSERT INTO clothes ( id, fk_resident_id, fk_pattern_id, fk_colour_name, density
         ( DEFAULT, NULL, 1, 'черный', 0, 'обувь' ),
         ( DEFAULT, NULL, 1, 'черный', 0, 'поножи' ),
         ( DEFAULT, NULL, 2, 'черный', 0, 'торс' );
-=======
-INSERT INTO clothes ( id, fk_resident_id, fk_pattern_id, fk_colour_id, density, fk_type_id )
-    VALUES
-        ( DEFAULT, NULL, 0, 0, 0, "обувь" ),
-        (),
-        ();
->>>>>>> 2ae5f5d2fbb5d04e0f74be5df4942ec63f2e8aea
+
