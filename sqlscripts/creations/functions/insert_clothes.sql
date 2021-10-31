ed609673688335b5eb53f19a8f5247c550cd6b61
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-10-31 15:05:28 +0300

    feat: insert clothes function

---
+++ sqlscripts/creations/functions/insert_clothes.sql
@@ -0,0 +1,39 @@
+CREATE OR REPLACE FUNCTION insert_clothes (
+    IN resident integer DEFAULT = NULL,
+    IN pattern integer DEFAULT = 0,
+    IN colour text DEFAULT = 'GreenYellow',
+    IN density integer DEFAULT = 0,
+    IN type_nm text DEFAULT = 'торс'
+) RETURNS void AS
+$BODY$
+    BEGIN
+        -- check if resident exists
+        IF resident IS NOT NULL AND NOT EXISTS( SELECT 1 FROM residents WHERE id = resident ) THEN
+            RAISE EXCEPTION "residents( % ) doesn't exist", resident;
+            -- TODO: insert non-existing row if it able
+        END IF;
+
+        -- check if pattern exists
+        IF EXISTS( SELECT 1 FROM patterns WHERE id = pattern ) THEN
+            RAISE EXCEPTION "patterns( % ) doesn't exist", pattern;
+            -- TODO: insert non-existing row if it able
+        END IF;
+
+        -- check if colour exists
+        IF EXISTS( SELECT 1 FROM colours WHERE name = colour ) THEN
+            RAISE EXCEPTION "colours( % ) doesn't exist", colour;
+            -- TODO: insert non-existing row if it able
+        END IF;
+
+        -- check if type exists
+        IF EXISTS( SELECT 1 FROM types WHERE name = type_nm ) THEN
+            RAISE EXCEPTION "types( % ) doesn't exist", type_nm;
+            -- TODO: insert non-existing row if it able
+        END IF;
+
+        -- otherwise insert valid data into table
+        INSERT INTO clothes ( id, fk_resident_id, fk_pattern_id, fk_colour_name, density, fk_type_name )
+            VALUES ( DEFAULT, resident, pattern, colour, density, type_nm );
+    END;
+$BODY$
+LANGUAGE 'plpgsql' VOLATILE;
---
+++ sqlscripts/drops/drop_functions.sql
@@ -0,0 +1,1 @@
+DROP FUNCTION IF EXISTS insert_clothes( ) CASCADE;
