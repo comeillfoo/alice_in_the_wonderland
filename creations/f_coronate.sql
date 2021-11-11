f340786192bc358769ba3599fca3d7e933cc90c6
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-11-12 02:44:39 +0300

    fix: indents in update

--- creations/f_coronate.sql
+++ creations/f_coronate.sql
@@ -5,8 +5,14 @@ $BODY$
     BEGIN
         DECLARE old_leader_id integer := ( SELECT id FROM leaders WHERE kingdom_reign_enddate IS NULL AND fk_kingdom_id = ( SELECT fk_kingdom_id FROM leaders WHERE id = new_king_id ) );
         DECLARE old_leader_crown_id integer := ( SELECT fk_crown_id FROM leaders WHERE id = old_leader_id ); 
-        UPDATE leaders SET kingdom_reign_enddate = CURRENT_DATE, fk_crown_id = NULL WHERE id = old_leader_id;
-        UPDATE leaders SET kingdom_reign_enddate = NULL, fk_crown_id = old_leader_crown_id WHERE id = new_leader_id;
+        UPDATE leaders
+        SET kingdom_reign_enddate = CURRENT_DATE,
+            fk_crown_id = NULL
+        WHERE id = old_leader_id;
+        UPDATE leaders
+        SET kingdom_reign_enddate = NULL,
+            fk_crown_id = old_leader_crown_id
+        WHERE id = new_leader_id;
     END;
 $BODY$
 LANGUAGE 'plpgsql' VOLATILE;
