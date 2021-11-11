a8cfd0bb2958156708081a3b140d4490322ad378
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-11-12 02:35:19 +0300

    fix: variable type

--- creations/f_coronate.sql
+++ creations/f_coronate.sql
@@ -3,8 +3,8 @@ CREATE OR REPLACE FUNCTION coronate (
 ) RETURNS void AS
 $BODY$
     BEGIN
-        old_leader_id integer := SELECT id FROM leaders WHERE kingdom_reign_enddate IS NULL AND fk_kingdom_id = ( SELECT fk_kingdom_id FROM leaders WHERE id = new_king_id );
-        old_leader_crown_id integer := SELECT fk_crown_id FROM leaders WHERE id = old_leader_id; 
+        old_leader_id leaders.id%ROWTYPE := SELECT id FROM leaders WHERE kingdom_reign_enddate IS NULL AND fk_kingdom_id = ( SELECT fk_kingdom_id FROM leaders WHERE id = new_king_id );
+        old_leader_crown_id leaders.fk_crown_id%ROWTYPE := SELECT fk_crown_id FROM leaders WHERE id = old_leader_id; 
         UPDATE leaders SET kingdom_reign_enddate = CURRENT_DATE, fk_crown_id = NULL WHERE id = old_leader_id;
         UPDATE leaders SET kingdom_reign_enddate = NULL, fk_crown_id = old_leader_crown_id WHERE id = new_leader_id;
     END;
