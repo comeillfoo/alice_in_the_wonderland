edd1ea062316aa7d33ff62fdb08f48242b415776
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-11-12 04:07:39 +0300

    fix: trying to put declarations aside

--- creations/f_coronate.sql
+++ creations/f_coronate.sql
@@ -2,16 +2,16 @@ CREATE OR REPLACE FUNCTION coronate (
     IN new_leader_id integer
 ) RETURNS void AS
 $BODY$
+    DECLARE
+        old_leader_id integer := ( SELECT id FROM leaders WHERE kingdom_reign_enddate IS NULL AND fk_kingdom_id = ( SELECT fk_kingdom_id FROM leaders WHERE id = new_king_id ) );
+        old_leader_crown_id integer := ( SELECT fk_crown_id FROM leaders WHERE id = old_leader_id );
     BEGIN
-        DECLARE old_leader_id integer := ( SELECT id FROM leaders WHERE kingdom_reign_enddate IS NULL AND fk_kingdom_id = ( SELECT fk_kingdom_id FROM leaders WHERE id = new_king_id ) );
-        DECLARE old_leader_crown_id integer := ( SELECT fk_crown_id FROM leaders WHERE id = old_leader_id );
-
         UPDATE leaders
-        SET ( kingdom_reign_enddate, fk_crown_id ) = ( CURRENT_DATE, NULL )
+        SET kingdom_reign_enddate = CURRENT_DATE, fk_crown_id = NULL
         WHERE id = old_leader_id;
 
         UPDATE leaders
-        SET ( kingdom_reign_enddate, fk_crown_id = ( NULL, old_leader_crown_id )
+        SET kingdom_reign_enddate = NULL, fk_crown_id = old_leader_crown_id
         WHERE id = new_leader_id;
     END;
 $BODY$
