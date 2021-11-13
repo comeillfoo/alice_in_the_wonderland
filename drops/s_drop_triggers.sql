0a4abf4a1baec3befe97ca66e82f05ac42e92469
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-11-13 21:57:46 +0300

    fix: remove triggers from dropping

--- drops/s_drop_triggers.sql
+++ drops/s_drop_triggers.sql
@@ -3,10 +3,6 @@ DROP TRIGGER IF EXISTS leader_suit_check on leaders CASCADE;
 
 DROP TRIGGER IF EXISTS resident_registration_check on residences CASCADE;
 
---DROP TRIGGER IF EXISTS tools_check CASCADE;
-
---DROP TRIGGER IF EXISTS weapons_check CASCADE;
-
 DROP TRIGGER IF EXISTS worships_check on worships CASCADE;
 
 
