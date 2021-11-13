ba77d3deb2634a662bd4dde8dbcaf07774dd2226
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-11-13 21:45:19 +0300

    fix: inserting tools and weapons

--- insertions/e_tools.sql
+++ insertions/e_tools.sql
@@ -1,3 +1,5 @@
 INSERT INTO tools ( id, name, fk_kingdom_id, fk_resident_id )
     VALUES
-        ( DEFAULT, 'пики точеные', 0,  );
+        ( DEFAULT, 'пики точеные'   , 1, 6 ),
+        ( DEFAULT, 'ножницы точеные', 1, 6 ),
+        ( DEFAULT, 'топор мясницкий', 1, 6 );
--- insertions/e_weapons.sql
+++ insertions/e_weapons.sql
@@ -1,3 +1,4 @@
 INSERT INTO weapons (id, name, fk_kingdom_id, fk_resident_id)
 	VALUES
-		(DEFAULT, 'xui drochenie prostite', 0, )
+		( DEFAULT, 'топор ударный', 3, 13 ),
+		( DEFAULT, 'моргенштерн'  , 3, 13 ),
