7a584a471990ef20c255477defb5c3628ab7006b
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-11-13 22:20:44 +0300

    feat: output all data

---
+++ selections/s_select_mode.sql
@@ -0,0 +1,37 @@
+-- entities for residents
+TABLE suits;
+TABLE sexes;
+TABLE roles;
+
+TABLE residents;
+TABLE gaits;
+TABLE kingdoms;
+
+TABLE registrations;
+
+
+TABLE residences;
+
+
+-- entities for clothes
+TABLE types;
+TABLE colours;
+TABLE patterns;
+
+TABLE clothes;
+
+
+TABLE crowns;
+
+
+
+TABLE leaders;
+
+
+TABLE worships;
+
+
+TABLE tools;
+
+
+TABLE weapons;
