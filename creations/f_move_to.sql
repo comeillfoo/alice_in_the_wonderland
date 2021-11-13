896e268a073bd80ef1f2f558feb4bcba414f5ec7
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-11-13 16:32:19 +0300

    fix: without using row type

--- creations/f_move_to.sql
+++ creations/f_move_to.sql
@@ -4,15 +4,15 @@ CREATE OR REPLACE FUNCTION move_to (
 ) RETURNS void AS
 $BODY$
     DECLARE
-        old_registration RECORD := ( SELECT * FROM registrations WHERE id IN ( SELECT fk_registration_id FROM residences WHERE fk_resident_id = resident ) AND expiry_date IS NULL );
+        old_registration_id integer := ( SELECT id FROM registrations WHERE id IN ( SELECT fk_registration_id FROM residences WHERE fk_resident_id = resident ) AND expiry_date IS NULL );
         new_registration_id integer;
     BEGIN
-        IF old_registration.fk_kingdom_id = dest_kingdom THEN
+        IF EXISTS( SELECT 1 FROM registrations WHERE id = old_registration_id AND fk_kingdom_id = dest_kingdom ) THEN
             RAISE INFO 'residents( % ) is already registrated in desired kingdom', resident;
             RETURN;
         END IF;
 
-        DELETE FROM residences WHERE fk_resident_id = resident AND fk_registration_id = old_registration.id;
+        DELETE FROM residences WHERE fk_resident_id = resident AND fk_registration_id = old_registration_id;
         new_registration_id := ( INSERT INTO registrations VALUES ( DEFAULT, dest_kingdom, now(), NULL ) RETURNING id );
         INSERT INTO residences VALUES ( resident, new_registration_id ); 
     END;
