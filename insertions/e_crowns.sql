9b120d3f5688447adce57d2158c4679d5fbc6e85
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-11-14 01:22:44 +0300

    fix: insertions into crowns

--- insertions/e_crowns.sql
+++ insertions/e_crowns.sql
@@ -1,7 +1,7 @@
 INSERT INTO crowns ( id, value, fk_clothes_id )
     VALUES
         ( DEFAULT, 106,  1 ),
-        ( DEFAULT, 597,  5 ),
-        ( DEFAULT,  65, 10 ),
-        ( DEFAULT, 137, 15 ),
-        ( DEFAULT, 510, 20 );
+        ( DEFAULT, 597,  6 ),
+        ( DEFAULT,  65, 11 ),
+        ( DEFAULT, 137, 16 ),
+        ( DEFAULT, 510, 21 );
