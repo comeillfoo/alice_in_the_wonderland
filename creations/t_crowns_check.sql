bd8b36b5f25eb666f1d75695a5e1e05aabf94d41
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-11-14 01:20:15 +0300

    fix: $ notation in function

--- creations/t_crowns_check.sql
+++ creations/t_crowns_check.sql
@@ -1,6 +1,4 @@
-CREATE OR REPLACE FUNCTION crowns_check() RETURNS trigger AS $crows_check$
-    DECLARE
-
+CREATE OR REPLACE FUNCTION crowns_check() RETURNS trigger AS $crowns_check$
     BEGIN
         IF ( 'головной убор' <> ( SELECT fk_type_id FROM clothes WHERE id=NEW.fk_clothes_id ) ) THEN
             RAISE EXCEPTION 'clothes ( % ) is not a hat', NEW.fk_clothes_id;
