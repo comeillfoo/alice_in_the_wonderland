c3f8e0de59389d6f75303286963b8f2a7903e915
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-11-13 21:45:58 +0300

    fix: ; in weapons

--- insertions/e_weapons.sql
+++ insertions/e_weapons.sql
@@ -1,4 +1,4 @@
 INSERT INTO weapons (id, name, fk_kingdom_id, fk_resident_id)
 	VALUES
 		( DEFAULT, 'топор ударный', 3, 13 ),
-		( DEFAULT, 'моргенштерн'  , 3, 13 ),
+		( DEFAULT, 'моргенштерн'  , 3, 13 );
