3b4cf58aca78e81e036e0c0605e14c6e8fd97bde
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-11-14 03:57:29 +0300

    fix: crowns_check for undefined owner

--- creations/t_crowns_check.sql
+++ creations/t_crowns_check.sql
@@ -2,8 +2,14 @@ CREATE OR REPLACE FUNCTION crowns_check(
 ) RETURNS trigger AS
 $crowns_check$
     DECLARE
+        maybe_cloth_owner integer := ( SELECT fk_resident_id FROM clothes WHERE id=NEW.fk_clothes_id );
         maybe_leader_role_name varchar( 24 ) := ( SELECT fk_role_name FROM residents WHERE id=( SELECT fk_resident_id FROM clothes WHERE id=NEW.fk_clothes_id ) );
     BEGIN
+        -- check if there is any point to check owner
+        IF ( maybe_cloth_owner IS NULL ) THEN
+            RETURN NEW;
+        END IF;
+
         -- check if cloth for crown has the proper type
         IF ( 'головной убор' <> ( SELECT fk_type_name FROM clothes WHERE id=NEW.fk_clothes_id ) ) THEN
             RAISE EXCEPTION 'clothes ( % ) is not a hat', NEW.fk_clothes_id;
