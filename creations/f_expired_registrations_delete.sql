8576b9ac45ca6cbb78fc22cc6ddb7988204f127f
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-11-15 00:24:27 +0300

    fix: trying using any

--- creations/f_expired_registrations_delete.sql
+++ creations/f_expired_registrations_delete.sql
@@ -6,7 +6,7 @@ $BODY$
     BEGIN
         DELETE FROM registrations WHERE expiry_date < CURRENT_DATE RETURNING id INTO removed_passes_id;
 
-        DELETE FROM residences WHERE fk_registration_id IN removed_passes_id; 
+        DELETE FROM residences WHERE fk_registration_id = ANY( removed_passes_id ); 
     END;
 $BODY$
 LANGUAGE 'plpgsql' VOLATILE;
