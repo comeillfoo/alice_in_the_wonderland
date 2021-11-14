56506b87996c662dc218281573055ab03a29c1ba
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-11-15 00:21:23 +0300

    fix: delete also residences

--- creations/f_expired_registrations_delete.sql
+++ creations/f_expired_registrations_delete.sql
@@ -1,8 +1,12 @@
 CREATE OR REPLACE FUNCTION expired_registrations_delete (
 ) RETURNS void AS
 $BODY$
+    DECLARE
+        removed_passes_id integer[];
     BEGIN
-        DELETE FROM registrations WHERE expiry_date < CURRENT_DATE CASCADE; 
+        DELETE FROM registrations WHERE expiry_date < CURRENT_DATE RETURNING id INTO removed_passes_id;
+
+        DELETE FROM residences WHERE fk_registration_id IN removed_passes_id; 
     END;
 $BODY$
 LANGUAGE 'plpgsql' VOLATILE;
