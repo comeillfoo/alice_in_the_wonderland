0572374e0f882ecadf91fa5d993fe645f1f31b18
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-11-14 21:54:37 +0300

    fix: saving results

--- benchmarks/f_perf_indeces.sql
+++ benchmarks/f_perf_indeces.sql
@@ -3,12 +3,13 @@ CREATE OR REPLACE FUNCTION perf_indeces (
 $BODY$
     DECLARE
         count integer := 1000;
+        some RECORD;
     BEGIN
         LOOP
-            SELECT * FROM leaders WHERE kingdom_reign_enddate IS NOT NULL;
-            SELECT * FROM clothes WHERE fk_type_name IN ( SELECT name FROM types );
-            SELECT * FROM tools WHERE fk_kingdom_id IN ( SELECT id FROM kingdoms );
-            SELECT * FROM weapons WHERE fk_kingdom_id IN ( SELECT id FROM kingdoms );
+            SELECT * FROM leaders WHERE kingdom_reign_enddate IS NOT NULL INTO some;
+            SELECT * FROM clothes WHERE fk_type_name IN ( SELECT name FROM types ) INTO some;
+            SELECT * FROM tools WHERE fk_kingdom_id IN ( SELECT id FROM kingdoms ) INTO some;
+            SELECT * FROM weapons WHERE fk_kingdom_id IN ( SELECT id FROM kingdoms ) INTO some;
             count := count - 1;
             IF count <= 0 THEN
                 EXIT;
