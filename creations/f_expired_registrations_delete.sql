1c0ec5a1a13ccd8e8894318c444a015937e44079
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-11-15 00:30:55 +0300

    fix: violation of foreign key

--- creations/f_expired_registrations_delete.sql
+++ creations/f_expired_registrations_delete.sql
@@ -2,11 +2,10 @@ CREATE OR REPLACE FUNCTION expired_registrations_delete (
 ) RETURNS void AS
 $BODY$
     DECLARE
-        removed_passes_id integer[];
+        expired_passes integer[] := ( SELECT id FROM registrations WHERE expiry_date < CURRENT_DATE );
     BEGIN
-        DELETE FROM registrations WHERE expiry_date < CURRENT_DATE RETURNING id INTO removed_passes_id;
-
-        DELETE FROM residences WHERE fk_registration_id = ANY( removed_passes_id ); 
+        DELETE FROM residences WHERE fk_registration_id = ANY( expired_passes ); 
+        DELETE FROM registrations WHERE id = ANY( expired_passes );
     END;
 $BODY$
 LANGUAGE 'plpgsql' VOLATILE;
