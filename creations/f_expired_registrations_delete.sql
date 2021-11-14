e271bfa0d0e7b9456ddb214462cb0563ceefd7d9
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-11-15 00:46:01 +0300

    fix: delete also from the registrations

--- creations/f_expired_registrations_delete.sql
+++ creations/f_expired_registrations_delete.sql
@@ -5,7 +5,7 @@ $BODY$
         expired_passes integer[] := ARRAY( SELECT id FROM registrations WHERE expiry_date < CURRENT_DATE );
     BEGIN
         DELETE FROM residences WHERE fk_registration_id = ANY( expired_passes ); 
-        -- DELETE FROM registrations WHERE id = ANY( expired_passes );
+        DELETE FROM registrations WHERE id = ANY( expired_passes );
     END;
 $BODY$
 LANGUAGE 'plpgsql' VOLATILE;
