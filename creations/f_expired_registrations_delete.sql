f750f1358cb3e449578d64be7d9135e486fb7dd3
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-11-15 00:44:55 +0300

    fix: another one initilize

--- creations/f_expired_registrations_delete.sql
+++ creations/f_expired_registrations_delete.sql
@@ -2,9 +2,8 @@ CREATE OR REPLACE FUNCTION expired_registrations_delete (
 ) RETURNS void AS
 $BODY$
     DECLARE
-        expired_passes integer[];
+        expired_passes integer[] := ARRAY( SELECT id FROM registrations WHERE expiry_date < CURRENT_DATE );
     BEGIN
-        SELECT id FROM registrations WHERE expiry_date < CURRENT_DATE INTO expired_passes;
         DELETE FROM residences WHERE fk_registration_id = ANY( expired_passes ); 
         -- DELETE FROM registrations WHERE id = ANY( expired_passes );
     END;
