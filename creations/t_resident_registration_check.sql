06c554fffe670c62b79dcec9f75d26f1b41f26f7
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-11-14 03:51:52 +0300

    fix: default value for trigger

--- creations/t_resident_registration_check.sql
+++ creations/t_resident_registration_check.sql
@@ -3,8 +3,10 @@ CREATE OR REPLACE FUNCTION resident_registration_check(
 $resident_registration_check$
     DECLARE
         resident_id integer := NEW.fk_resident_id;
-        resident_registration registrations := ( SELECT * FROM registrations WHERE id = NEW.fk_registration_id );
+        resident_registration registrations;
     BEGIN
+        SELECT * FROM registrations WHERE id = NEW.fk_registration_id INTO resident_registration;
+        
         IF ( resident_registration.expiry_date IS NULL ) THEN
             IF ( EXISTS( SELECT * FROM residences JOIN registrations ON residences.fk_registration_id = registrations.id WHERE fk_resident_id = resident_id AND expiry_date IS NULL ) ) THEN
                 RAISE EXCEPTION 'residents ( % ) already has permanent residence permit, please withdraw the old one', resident_id;
