563a50ef37a676906aa59091ee300bb2150ec2c8
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-11-22 03:24:17 +0300

    feat: try not using order by

--- creations/s_create_views.sql
+++ creations/s_create_views.sql
@@ -1,1 +1,6 @@
-CREATE OR REPLACE VIEW leaders_show_generations AS SELECT kingdoms.fk_suit_name, leaders.kingdom_reign_enddate, residents.name, residents.fk_sex_name FROM leaders JOIN residents ON leaders.fk_resident_id = residents.id JOIN kingdoms ON leaders.fk_kingdom_id = kingdoms.id GROUP BY leaders.kingdom_reign_enddate, kingdoms.fk_suit_name, residents.name, residents.fk_sex_name ORDER BY kingdoms.fk_suit_name ASC, leaders.kingdom_reign_enddate DESC;
+CREATE OR REPLACE VIEW leaders_show_generations AS
+    SELECT kingdoms.fk_suit_name, leaders.kingdom_reign_enddate, residents.name, residents.fk_sex_name
+        FROM leaders
+            JOIN residents ON leaders.fk_resident_id = residents.id
+            JOIN kingdoms ON leaders.fk_kingdom_id = kingdoms.id
+            ORDER BY kingdoms.fk_suit_name, leaders.kingdom_reign_enddate;
