c3a866ddbf358b0a942ec5a0c6b9b3bd5f2d648a
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-11-22 03:25:10 +0300

    fix: order of dates

--- creations/s_create_views.sql
+++ creations/s_create_views.sql
@@ -3,4 +3,4 @@ CREATE OR REPLACE VIEW leaders_show_generations AS
         FROM leaders
             JOIN residents ON leaders.fk_resident_id = residents.id
             JOIN kingdoms ON leaders.fk_kingdom_id = kingdoms.id
-            ORDER BY kingdoms.fk_suit_name, leaders.kingdom_reign_enddate;
+            ORDER BY kingdoms.fk_suit_name, leaders.kingdom_reign_enddate DESC;
