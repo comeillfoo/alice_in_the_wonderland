75bb7f16e804d6c4583660d593590bcc2db2ada1
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-11-22 03:30:25 +0300

    fix: specify the table for select

--- creations/s_create_views.sql
+++ creations/s_create_views.sql
@@ -6,7 +6,7 @@ CREATE OR REPLACE VIEW leaders_show_generations AS
             ORDER BY kingdoms.fk_suit_name, leaders.kingdom_reign_enddate DESC;
 
 CREATE OR REPLACE VIEW kingdoms_count_tools AS
-    SELECT tools.fk_kingdom_id, kingdoms.fk_suit_name, count( id ) AS tools_number
+    SELECT tools.fk_kingdom_id, kingdoms.fk_suit_name, count( tools.id ) AS tools_number
         FROM tools
             JOIN kingdoms ON tools.fk_kingdom_id = kingdoms.id
             GROUP BY fk_kingdom_id;
