01a0580b7498933198b216b97d159ec39887931c
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-12-10 04:28:20 +0300

    feat: completed id for views

--- creations/s_create_views.sql
+++ creations/s_create_views.sql
@@ -1,12 +1,12 @@
 CREATE OR REPLACE VIEW leaders_show_generations AS
-    SELECT kingdoms.fk_suit_name, leaders.kingdom_reign_enddate, residents.name, residents.fk_sex_name
+    SELECT row_number() over() as id, kingdoms.fk_suit_name, leaders.kingdom_reign_enddate, residents.name, residents.fk_sex_name
         FROM leaders
             JOIN residents ON leaders.fk_resident_id = residents.id
             JOIN kingdoms ON leaders.fk_kingdom_id = kingdoms.id
             ORDER BY kingdoms.fk_suit_name, leaders.kingdom_reign_enddate DESC;
 
 CREATE OR REPLACE VIEW leaders_count_courtiers AS
-    SELECT residents.name, count( worships.fk_courtier_id ) AS courtier
+    SELECT row_number() over() as id, residents.name, count( worships.fk_courtier_id ) AS courtier
         FROM worships
             JOIN leaders ON leaders.id = worships.fk_leader_id
             JOIN residents ON leaders.fk_resident_id = residents.id
