4bc9a4d1464a4ce28e4a1384d380dc330152cb86
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-12-29 02:46:04 +0300

    fix: visit according to hibernate

--- creations/f_visit_kingdom.sql
+++ creations/f_visit_kingdom.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION visit_kingdom (
     IN resident integer,
     IN spandays integer,
     IN dest_kingdom integer
-) RETURNS void AS
+) RETURNS integer AS
 $BODY$
     DECLARE
         is_already_live_here boolean := EXISTS( SELECT 1 FROM registrations WHERE fk_kingdom_id = dest_kingdom AND ( expiry_date IS NULL OR expiry_date > CURRENT_DATE ) AND id IN ( SELECT fk_registration_id FROM residences WHERE fk_resident_id = resident ) );
@@ -19,6 +19,7 @@ $BODY$
 
         INSERT INTO registrations VALUES ( DEFAULT, dest_kingdom, CURRENT_DATE, CURRENT_DATE + spandays * INTERVAL '1 day' ) RETURNING id INTO guest_pass_id;
         INSERT INTO residences VALUES ( resident, guest_pass_id );
+        RETURN 1;
     END;
 $BODY$
 LANGUAGE 'plpgsql' VOLATILE;
