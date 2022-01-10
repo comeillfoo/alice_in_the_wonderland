84b5f6e4e7be0cfb4b15bb7527b77fcc29fd366c
MikEKun0 <lavlinskiy2001@mail.ru>
2022-01-11 02:20:49 +0300

    fixed returned value and field

--- creations/f_resident_move.sql
+++ creations/f_resident_move.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE FUNCTION resident_move (
     IN resident integer,
     IN dest_kingdom integer
-) RETURNS void AS
+) RETURNS integer AS
 $BODY$
     DECLARE
         old_registration_id integer := ( SELECT id FROM registrations WHERE id IN ( SELECT fk_registration_id FROM residences WHERE fk_resident_id = resident ) AND expiry_date IS NULL );
@@ -10,8 +10,7 @@ $BODY$
         tool RECORD;
     BEGIN
         IF EXISTS( SELECT 1 FROM registrations WHERE id = old_registration_id AND fk_kingdom_id = dest_kingdom ) THEN
-            RAISE INFO 'residents( % ) is already registered in desired kingdom', resident;
-            RETURN;
+            RAISE EXCEPTION 'residents( % ) is already registered in desired kingdom', resident;
         END IF;
 
         DELETE FROM residences WHERE fk_resident_id = resident AND fk_registration_id = old_registration_id;
@@ -19,7 +18,7 @@ $BODY$
         INSERT INTO residences VALUES ( resident, new_registration_id );
         DELETE FROM residences WHERE fk_resident_id = resident AND fk_registration_id IN ( SELECT id FROM registrations WHERE fk_kingdom_id = dest_kingdom AND expiry_date IS NOT NULL );
 
-        IF ( 'солдат' = ( SELECT fk_role_id FROM residents WHERE id = resident ) ) THEN
+        IF ( 'солдат' = ( SELECT fk_role_name FROM residents WHERE id = resident ) ) THEN
             -- move all weapons
             FOR weapon IN SELECT * FROM weapons WHERE fk_resident_id = resident
             LOOP
@@ -27,13 +26,14 @@ $BODY$
             END LOOP;
         END IF;
 
-        IF ( 'садовник' = ( SELECT fk_role_id FROM residents WHERE id = resident ) ) THEN
+        IF ( 'садовник' = ( SELECT fk_role_name FROM residents WHERE id = resident ) ) THEN
             -- move all tools
             FOR tool IN SELECT * FROM tools WHERE fk_resident_id = resident
             LOOP
                 PERFORM tool_move( resident, tool.id, dest_kingdom );
             END LOOP;
         END IF;
+	RETURN 1;
     END;
 $BODY$
 LANGUAGE 'plpgsql' VOLATILE;
