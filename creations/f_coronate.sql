81bcaad702ad3f5aa1a79c8406241917fdcd83f5
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2022-01-11 19:30:32 +0300

    fix: coronate for multiple leaders

--- creations/f_coronate.sql
+++ creations/f_coronate.sql
@@ -3,15 +3,15 @@ CREATE OR REPLACE FUNCTION coronate (
 ) RETURNS integer AS
 $BODY$
     DECLARE
-        old_leader_id integer := ( SELECT id FROM leaders WHERE kingdom_reign_enddate IS NULL AND fk_kingdom_id = ( SELECT fk_kingdom_id FROM leaders WHERE id = new_leader_id ) );
-        old_leader_crown_id integer := ( SELECT fk_crown_id FROM leaders WHERE id = old_leader_id );
+        old_leader_ids integer[] := ARRAY( SELECT id FROM leaders WHERE kingdom_reign_enddate IS NULL AND fk_kingdom_id = ( SELECT fk_kingdom_id FROM leaders WHERE id = new_leader_id ) );
+        -- old_leader_crown_ids integer[] := ARRAY( SELECT fk_crown_id FROM leaders WHERE id IN old_leader_id );
     BEGIN
         UPDATE leaders
-        SET kingdom_reign_enddate = CURRENT_DATE, fk_crown_id = NULL
-        WHERE id = old_leader_id;
+        SET kingdom_reign_enddate = CURRENT_DATE
+        WHERE id = ANY( old_leader_ids );
 
         UPDATE leaders
-        SET kingdom_reign_enddate = NULL, fk_crown_id = old_leader_crown_id
+        SET kingdom_reign_enddate = NULL
         WHERE id = new_leader_id;
 
         RETURN 1;
